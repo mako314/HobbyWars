ad2e4ab126ee7581e5b5f732406f976fbec2ffdc
Macolister <bispo.mac@gmail.com>
2023-07-27 14:57:49 -0400

    skeleton app and seeds

---
+++ server/seeds.py
@@ -0,0 +1,11 @@
+from random import choice as rc, randrange
+
+from app import app
+from models import db, User, Hobby, Competition, Result, Entry
+
+from random import randint, choice as rc
+import pandas as pd
+
+if __name__ == '__main__':
+    with app.app_context():
+        pass

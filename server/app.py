4185cd29846b644da52fbb45955f1968bbecc9c3
Macolister <bispo.mac@gmail.com>
2023-07-30 18:21:44 -0400

    comment out config file

--- server/app.py
+++ server/app.py
@@ -1,10 +1,9 @@
 from models import db, User, Hobby, UserHobby, Competition, Result, Entry
+from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask import Flask, request, make_response, jsonify, session
 from flask_restful import Api, Resource
-from flask_bcrypt import Bcrypt
-
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))

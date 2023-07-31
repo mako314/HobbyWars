213cbcbfc158d324e8490c602ec2120e3bb1f704
Macolister <bispo.mac@gmail.com>
2023-07-31 18:50:47 -0400

    add routing, useffect for comps

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -3,16 +3,18 @@ import React from 'react';
 function HomePage({}) {
 
 
-
-
-
-
-
-
     return (
         <div>
-
-        </div>
+            <section className="text-gray-400 bg-gray-900 body-font">
+            <div className="container px-5 py-24 mx-auto flex flex-col">
+                <div className="lg:w-4/6 mx-auto">
+                <div className="rounded-lg h-64 overflow-hidden">
+                    <img alt="content" className="object-cover object-center h-full w-full" src="/home/mako77/code/Flatiron/Projects/HobbyWars/client/public/hobby.png"/>
+                </div>
+                </div>
+            </div>
+        </section>
+    </div>
     )
 }
 
